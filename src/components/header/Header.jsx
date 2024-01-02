4ea3d012d9569feee17ed0134c132ab3060a4e0b
Karla Goo <karla_goo@yahoo.com>
2024-01-02 14:13:05 -0600

    header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,45 +3,39 @@ import "./header.css";
 
 function Header() {
   return (
-    <header>
-      <nav className="header">
+    <header className="header">
+      <nav className="nav container">
         <a href="index.html" className="nav__logo">
           Karla Goo
         </a>
 
         <div className="nav__menu">
-          <ul>
-            <li className="nav__list grid">
+          <ul className="nav__list grid">
+            <li className="nav__item">
               <a href="#home" className="nav__link">
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
 
-            <li className="nav__list grid">
+            <li className="nav__item">
               <a href="#about" className="nav__link">
                 <i className="uil uil-user nav__icon"></i> About Me
               </a>
             </li>
 
-            <li className="nav__list grid">
-              <a href="" className="nav__link">
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>
             </li>
 
-            <li className="nav__list grid">
-              <a href="#skills" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
-              </a>
-            </li>
-
-            <li className="nav__list grid">
+            <li className="nav__item">
               <a href="#portfolio" className="nav__link">
                 <i className="uil uil-scenery nav__icon"></i> Portfolio
               </a>
             </li>
 
-            <li className="nav__list grid">
+            <li className="nav__item">
               <a href="#contact" className="nav__link">
                 <i className="uil uil-message nav__icon"></i> Contact
               </a>
