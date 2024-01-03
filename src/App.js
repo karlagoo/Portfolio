157f5a166857b0fe009ba5a4303f212e6bd71f4c
Karla Goo <karla_goo@yahoo.com>
2024-01-02 21:01:44 -0600

    created home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Karla Goo</h1>
+      <h3 className="home__subtitle">Full Stack Web Developer</h3>
+      <p className="home__description">
+        I am a Northwestern coding bootcamp alumnus (graduated in November 2021)
+        turned versatile full-stack web developer, turned technical product
+        manager crafting seamless digital experiences! Beyond lines of code and
+        product development I'm a culinary maestro in the kitchen and a fitness
+        enthusiast, finding the perfect blend of creativity and discipline in
+        code, business and cuisine.
+      </p>
+    </div>
+  );
+}
+
+export default Data
