4bd7f8da191e3bd95f4e20c7fc085a40e6c20cbc
Karla Goo <karla_goo@yahoo.com>
2024-01-03 09:56:29 -0600

    about me styling

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,16 +4,19 @@ function Info() {
   return (
     <div className="about__info grid">
       <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">1.5 Years Working</span>
       </div>
 
       <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">6 + Projects</span>
       </div>
 
       <div className="about__box">
+        <i class="bx bx-world about__icon"></i>
         <h3 className="about__title">Bilingual</h3>
         <span className="about__subtitle">English & Spanish</span>
       </div>
