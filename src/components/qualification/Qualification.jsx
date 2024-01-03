c5f2e65dca590c9a6a21c58df09270578e10116b
Karla Goo <karla_goo@yahoo.com>
2024-01-03 14:19:20 -0600

    qualification styling

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./qualification.css";
 
 function Qualification() {
+
+    const[toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
   return (
     <section className="qualification section">
-      <h2 className="section__title">Qualification</h2>
+      <h2 className="section__title">Qualifications</h2>
       <span className="section__subtitle">My Personal Journey</span>
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={(e) => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__button button--flex">
-            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={(e) => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content qualification__content-active">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Bachelors in Marketing</h3>
@@ -95,13 +122,17 @@ function Qualification() {
             </div>
           </div>
 
-          <div className="qualification__content">
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Job 1</h3>
-                <span className="qualification__subtitle">
-                  Job Location 1
-                </span>
+                <span className="qualification__subtitle">Job Location 1</span>
                 <div className="qualification__calendar">
                   <i className="uil uil-calendar-alt"></i> 2012 - 2016
                 </div>
@@ -122,12 +153,8 @@ function Qualification() {
               </div>
 
               <div>
-                <h3 className="qualification__title">
-                  Job 2
-                </h3>
-                <span className="qualification__subtitle">
-                  Job Location 2
-                </span>
+                <h3 className="qualification__title">Job 2</h3>
+                <span className="qualification__subtitle">Job Location 2</span>
                 <div className="qualification__calendar">
                   <i className="uil uil-calendar-alt"></i> 2021
                 </div>
