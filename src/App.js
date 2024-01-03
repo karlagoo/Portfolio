18bbb06c8d4721e730b3b9be09248ec18a3d0c2f
Karla Goo <karla_goo@yahoo.com>
2024-01-03 15:33:03 -0600

    contact form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home.jsx';
 import About from './components/about/About.jsx';
 import Skills from './components/skills/Skills.jsx';
 import Qualification from './components/qualification/Qualification.jsx';
+import Contact from './components/contact/Contact.jsx';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Qualification />
+        <Contact />
       </main>
     </div>
   );
