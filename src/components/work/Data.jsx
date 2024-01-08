ddd90ffc970828d0ad7e89c85af883a6beca7934
Karla Goo <karla_goo@yahoo.com>
2024-01-07 21:43:34 -0600

    added project links

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -5,36 +5,41 @@ import Project4 from '../../assets/star-spangled-memer.jpg';
 import Project5 from '../../assets/weatherapp.jpg';
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Project1,
-        title: 'Tastyyy',
-        category: 'React app',
-    },
-    {
-        id: 2,
-        image: Project2,
-        title: 'Enlace',
-        category: 'MERN Stack',
-    },
-    {
-        id: 3,
-        image: Project3,
-        title: 'Nanny Poppins',
-        category: 'Web App',
-    },
-    {
-        id: 4,
-        image: Project4,
-        title: 'Star Spangled Memer',
-        category: 'API',
-    },
-    {
-        id: 5,
-        image: Project5,
-        title: 'Weather App',
-        category: 'API',
-    },
+  {
+    id: 1,
+    image: Project1,
+    title: "Tastyyy",
+    category: "React app",
+    link: "https://tastyyy-636317356501.herokuapp.com/",
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: "Enlace",
+    category: "MERN Stack",
+    link: "https://enlace.herokuapp.com/",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Nanny Poppins",
+    category: "Web App",
+    link: "https://nanny-poppins.herokuapp.com/",
+  },
+  {
+    id: 4,
+    image: Project4,
+    title: "Star Spangled Memer",
+    category: "API",
+    link: "https://p1t1-web-dev-wizards.github.io/Project1---horoscope-memes/",
+  },
+  {
+    id: 5,
+    image: Project5,
+    title: "Weather App",
+    category: "API",
+    link: "https://karlagoo.github.io/Weather-Dashboard/",
+  },
 ];
 
 export const projectsNav = [
